5cd1317b357cd160c21fa577d13701994a321e46
lakshanm <lakshan.mihiranga@Directfn.com>
2025-02-25 00:25:38 +0530

    Finalizing home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,47 @@
+import { useState, useEffect } from 'react';
+import '../css/Home.css';
 import MovieCard from '../components/MovieCard';
+import {getPopularMovies, searchMovies} from '../services/api';
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        {id: 1, title: "John Wick", release_date: "2023" },
-        {id: 2, title: "Harry Potter", release_date: "2012" },
-        {id: 3, title: "Extraction", release_date: "2019" },
-    ]
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            } catch (err) {
+                console.log(err);
+                setError("Failed to load movies...");
+            }
+            finally {
+                setLoading(false);
+            }
+        }
 
-    const handleSearch = () => {
-        alert("Search");
+        loadPopularMovies();
+    }, [])
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        if (!searchQuery.trim()) return;
+        if (loading) return;
+
+        setLoading(true);
+        try {
+            const searchResults = await searchMovies(searchQuery);
+            setMovies(searchResults);
+            setError(null)
+        } catch (err) {
+            console.log(err);
+            setError("Failed to load movies");
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
@@ -19,14 +51,21 @@ function Home() {
                 type='text'
                 placeholder='Search movies...'
                 className='search-input'
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
                 />
                 <button type='submit' className='search-button'>Search</button>
             </form>
+            {error && <div className='error-message'>{error}</div>}
+            {loading ? 
+            <div className='loading'>Loading...</div> 
+            : 
             <div className="movies-grid">
                 {movies.map((movie) => (
                     <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
+            }
         </div>
     )
 }
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "0e8459ba965231dfb6f42b1a44ad28e1"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
